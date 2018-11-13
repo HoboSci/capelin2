45b98338dcb2dbaed3ccb6b5938dde159be5cda0
Samantha Andrews <11350936+oceanoculus@users.noreply.github.com>
2018-11-13 13:15:45 +0100

    removing some test scripts i no longer need, plus data exploration pt 2

--- src/rasterbrick_to_ae.R
+++
@@ -1,7 +0,0 @@
-# project rasterbricks into albers equal area based on a tiff that has been reprojected already (called aea)
-
-rasterbrick_to_aea <- function( brick_input ) {
-  brick_input %>%
-    projectRaster(. , proj4string(aea)) %>%
-    return(.)
-}
