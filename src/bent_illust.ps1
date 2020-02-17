8d125f432ade1e3f1fa81b7a7b8ef0b0382ec596
Samantha Andrews <11350936+oceanoculus@users.noreply.github.com>
2020-02-17 16:07:08 +0100

    building appendix example (bent)

---
+++ src/bent_illust.ps1
@@ -0,0 +1,34 @@
+﻿#need to change source folder
+#need to change filter
+
+
+#o2
+#temp
+#salinity
+
+$source="C:\Users\bunny\Documents\GitHub\Chapter_1\output\maxent\paper_plots\bent_illus\sal" #location of starting directory
+$dest = "C:\cygwin64\home\bunny\ASCII"; #location where files will be copied to
+$filter = "salinity" #  start 0
+
+
+#empty the destimation folder
+Get-ChildItem -Path C:\cygwin64\home\bunny\ASCII -Include *.* -File -Recurse | foreach { $_.Delete()}
+
+
+#move files
+
+Get-ChildItem -Path $source | Where-Object { $_.Name -match $filter } | Copy-Item -Destination $dest
+
+
+#rename the files for merging
+
+Set-Location "C:\cygwin64\home\bunny\ASCII"
+get-childitem *.asc| foreach { rename-item $_ $_.Name.Replace("9.82275", "101")}
+get-childitem *.asc| foreach { rename-item $_ $_.Name.Replace("221.141", "102")}
+get-childitem *.asc| foreach { rename-item $_ $_.Name.Replace("508.64", "103")}
+
+
+
+
+
+
